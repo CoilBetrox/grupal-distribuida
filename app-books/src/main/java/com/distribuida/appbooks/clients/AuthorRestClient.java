70532e60f405c632f6ab07621594d1ba9f5eed7e
Dennis Pilco <39228294+dennisyaj@users.noreply.github.com>
2024-02-22 17:00:57 -0500

    cambios app books

--- app-books/src/main/java/com/distribuida/appbooks/clients/AuthorRestClient.java
+++ app-books/src/main/java/com/distribuida/appbooks/clients/AuthorRestClient.java
@@ -3,17 +3,15 @@ package com.distribuida.appbooks.clients;
 import com.distribuida.appbooks.dtos.AuthorDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Service
+@Component
 public class AuthorRestClient {
-
-
     private final RestTemplate restTemplate;
     private final String authorsUrl;
 
-    @Autowired
     public AuthorRestClient(RestTemplate restTemplate, @Value("${app.authors.url}") String authorsUrl) {
         this.restTemplate = restTemplate;
         this.authorsUrl = authorsUrl;
